b0d50c51457d9ed03ccf77ddbc8f9a261bae4488
Ollivier Julles R. Morales <morales.ollivier@gmail.com>
2020-03-10 21:04:54 +0800

    Backend finished

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config'); // default.json
+const db = config.get('mongoURI'); // get any values inside defaul.json
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
